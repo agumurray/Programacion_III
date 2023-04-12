96de02ad03d6226bec8a61e7e7cf1bb346a259a2
Agustin Murray <agumurray@gmail.com>
2023-04-11 21:16:56 -0300

    practica 3 unions-enums-campos

---
+++ Practica4B/Ej4/Test.java
@@ -0,0 +1,22 @@
+public class Test {
+    public static void main(String[] args) {
+        ArbolBinario<Integer> arbolBinarioB=new ArbolBinario<Integer>(1);
+        ArbolBinario<Integer> hijoIzquierdoB=new ArbolBinario<Integer>(2);
+        hijoIzquierdoB.agregarHijoIzquierdo(new ArbolBinario<Integer>(3));
+        hijoIzquierdoB.agregarHijoDerecho(new ArbolBinario<Integer>(4));
+        ArbolBinario<Integer> hijoDerechoB=new ArbolBinario<Integer>(6);
+        hijoDerechoB.agregarHijoIzquierdo(new ArbolBinario<Integer>(7));
+        hijoDerechoB.agregarHijoDerecho(new ArbolBinario<Integer>(8));
+        arbolBinarioB.agregarHijoIzquierdo(hijoIzquierdoB);
+        arbolBinarioB.agregarHijoDerecho(hijoDerechoB);
+
+        arbolBinarioB.printPreorden();
+        System.out.println("Fin arbol");
+
+        ListaGenericaEnlazada<Integer> trayectoriasPesadas = new ListaGenericaEnlazada<Integer>();
+
+        trayectoriasPesadas = arbolBinarioB.trayectoriaPesada(arbolBinarioB);
+
+        System.out.println(trayectoriasPesadas);
+    }
+}
